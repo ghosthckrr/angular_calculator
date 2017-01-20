da798e14a039516285c2630a1f068a675f8a8aec
ghosthckrr <imashishjoshi@gmail.com>
2017-01-20 23:04:10 +0530

    Working simply

--- src/index.js
+++ src/index.js
@@ -7,11 +7,58 @@ app.controller('mycontroller',($scope)=>{
  let buttons =[7,8,9,4,5,6,1,2,3,0];
  let operations = ["/","x","-","+"]; 
   $scope.btns = buttons;
-  $scope.display = "89858";
+  
   $scope.operations = operations;
 
+  let value = 0;
+  let tvalue = 0;
+  let op = 'none';
+  $scope.display = value;
   $scope.btnclick = (e)=>{
+  	value = value*10+e;
+  	$scope.display = value;
+  };
+  $scope.opt = (o) =>{
+  	tvalue = value;
+  	value = 0;
+  	op = o;
   	
+  };
+  $scope.equal = () =>{
+  	switch(op){
+  		case '-':
+  			$scope.display = tvalue-value;
+  			value = 0;
+  			tvalue = 0;
+  			break;
+
+  		case '+':
+  			$scope.display = tvalue+value;
+  			value = 0;
+  			tvalue = 0;
+  			break;	
+		case 'x':
+			$scope.display = tvalue*value;
+			value = 0;
+  			tvalue = 0;
+			break;
+
+  		case '/':
+  			$scope.display = tvalue/value;
+  			value = 0;
+  			tvalue = 0;
+  			break;	
+
+  		case 'none':
+  			$scope.display = value;
+  			break;		
+  	}
+
+  };
+  $scope.clear =()=>{
+  	value = 0;
+  	tvalue = 0;
+  	$scope.display = value;
   };
 
 });
