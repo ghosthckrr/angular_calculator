21805d5f288d2dea43799c711b923c4ac8536c25
ghosthckrr <imashishjoshi@gmail.com>
2017-01-20 22:41:31 +0530

    calculator ui done!

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import './css/main.scss';
 
 const app = angular.module('myapp',[]);
 app.controller('mycontroller',($scope)=>{
-  $scope.name = "Working!!";
+
+ let buttons =[7,8,9,4,5,6,1,2,3,0];
+ let operations = ["/","x","-","+"]; 
+  $scope.btns = buttons;
+  $scope.display = "89858";
+  $scope.operations = operations;
+
+  $scope.btnclick = (e)=>{
+  	
+  };
+
 });
