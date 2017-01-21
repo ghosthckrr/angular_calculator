31c34980a55937f057def877b1bf5a43cf8e35cc
ghosthckrr <imashishjoshi@gmail.com>
2017-01-21 13:55:05 +0530

    decimal functionality

--- src/index.js
+++ src/index.js
@@ -5,26 +5,47 @@ const app = angular.module('myapp',[]);
 app.controller('mycontroller',($scope)=>{
 
  let buttons =[7,8,9,4,5,6,1,2,3,0];
- let operations = ["/","x","-","+"]; 
+ let operations = ["/","x","-","+"];
   $scope.btns = buttons;
-  
+
   $scope.operations = operations;
 
   let value = 0;
   let tvalue = 0;
   let op = 'none';
+  let dvalue;
+  let fdvalue;
+  var decimal = false;
   $scope.display = value;
   $scope.btnclick = (e)=>{
-  	value = value*10+e;
+    value = value*10+e;
+  	if(!decimal){
   	$scope.display = value;
+  }else{
+    fdvalue = dvalue+value;
+    $scope.display = fdvalue;
+  }
   };
   $scope.opt = (o) =>{
-  	tvalue = value;
-  	value = 0;
   	op = o;
-  	
+    if(decimal){
+      tvalue = parseFloat(fdvalue);
+      fdvalue = '';
+      value =0;
+      decimal = false;
+    }else{
+      tvalue = value;
+      value = 0;
+    }
+
   };
   $scope.equal = () =>{
+
+    if(decimal){
+      value = parseFloat(fdvalue);
+      console.log(value);
+    }
+
   	switch(op){
   		case '-':
   			$scope.display = tvalue-value;
@@ -36,7 +57,7 @@ app.controller('mycontroller',($scope)=>{
   			$scope.display = tvalue+value;
   			value = 0;
   			tvalue = 0;
-  			break;	
+  			break;
 		case 'x':
 			$scope.display = tvalue*value;
 			value = 0;
@@ -47,17 +68,25 @@ app.controller('mycontroller',($scope)=>{
   			$scope.display = tvalue/value;
   			value = 0;
   			tvalue = 0;
-  			break;	
+  			break;
 
   		case 'none':
   			$scope.display = value;
-  			break;		
+  			break;
   	}
 
+  };
+  $scope.decimal =()=>{
+    decimal = true;
+  	dvalue = value + ".";
+    value=0;
+    $scope.display = dvalue;
   };
   $scope.clear =()=>{
   	value = 0;
   	tvalue = 0;
+    dvalue =0;
+    decimal = false;
   	$scope.display = value;
   };
 
